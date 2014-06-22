a8ba9ef1b9add05734024f12c88520cd9521fcff
Jorge Ayuso <jayusor@gmail.com>
2014-06-22 15:32:11 +0200

    Configure cluster

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,9 +10,15 @@ sudo ufw disable
 
 
 sudo apt-get update
+sudo apt-get upgrade -y
 sudo apt-get install zookeeperd default-jre python-setuptools python-protobuf curl -y
 curl -sSfL http://downloads.mesosphere.io/master/ubuntu/14.04/mesos_0.19.0~ubuntu14.04%2B1_amd64.deb --output /tmp/mesos.deb
 sudo dpkg -i /tmp/mesos.deb
 curl -sSfL http://downloads.mesosphere.io/master/ubuntu/14.04/mesos-0.19.0_rc2-py2.7-linux-x86_64.egg --output /tmp/mesos.egg
 sudo easy_install /tmp/mesos.egg
+
+sudo chmod 777 /etc/mesos/zk
+echo "zk://u1401.devel.conento.com:2181/mesos" > /etc/mesos/zk
+sudo chmod 644 /etc/mesos/zk
+
 sudo reboot
